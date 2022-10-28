fd6cb922fe48503fac20f65552278881958dbadf
MGulyagari <116884327+MGulyagari@users.noreply.github.com>
2022-10-28 12:08:27 +0100

    -updated resource name

--- Git-work/tf-alarm-main/route53.tf
+++ Git-work/tf-alarm-main/route53.tf
@@ -1,6 +1,6 @@
 # Private Hosted Zone
 
-resource "aws_route53_zone" "bta" {
+resource "aws_route53_zone" "adobe_proxy" {
   name = var.route53_zone
   vpc {
     vpc_id = data.terraform_remote_state.bt-vpc.outputs.vpc_id
